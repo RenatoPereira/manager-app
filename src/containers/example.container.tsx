11604729285577b9ba965b39cc6856cc23c4988c
RenatoPereira <renatopereira.wp@gmail.com>
2024-12-24 10:14:46 -0300

    feat(playwright): add playwirght

--- src/containers/example.container.tsx
+++ src/containers/example.container.tsx
@@ -5,7 +5,7 @@ import { ExampleComponent } from '@/components/example.component'
 export const ExampleContainer = () => {
   const t = useTranslations('HomePage')
   return (
-    <div className="flex flex-col gap-y-10 items-center justify-center min-h-screen">
+    <div className="flex flex-col gap-y-10 items-center justify-center min-h-full">
       <h1 className="text-5xl font-bold">{t('title')}</h1>
 
       <ExampleComponent />
