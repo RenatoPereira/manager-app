3a3fa663d64466466db25884a1a0d119673dd8c7
RenatoPereira <renatopereira.wp@gmail.com>
2024-12-24 10:40:18 -0300

    feat(playwright): fix CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig, devices } from '@playwright/test'
 
+const PORT = process.env.PORT || 3000
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -26,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
@@ -37,23 +40,23 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
-    },
+    }
 
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] }
     // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] }
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] }
-    }
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] }
+    // }
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] }
@@ -68,12 +71,13 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'node .next/standalone/server.js',
+    url: BASE_URL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI
+  }
 })
