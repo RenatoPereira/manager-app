aafe30645ea81cf76bb833f26c2694c4414faeb3
RenatoPereira <renatopereira.wp@gmail.com>
2024-12-25 23:02:52 -0300

    test(darkmode): add test for darkmode dark class

--- src/components/darkmode.component.test.tsx
+++ src/components/darkmode.component.test.tsx
@@ -4,7 +4,7 @@ import { DarkModeComponent } from './darkmode.component'
 
 const mockMatchMedia = (matches: boolean) => ({
   matches,
-  media: '(prefers-color-scheme: dark)',
+  media: `(prefers-color-scheme: ${matches ? 'dark' : 'light'})`,
   addListener: jest.fn(),
   removeListener: jest.fn(),
   addEventListener: jest.fn(),
@@ -17,8 +17,8 @@ beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
     writable: true,
     value: jest.fn().mockImplementation((query) => {
-      if (query === '(prefers-color-scheme: dark)') {
-        return mockMatchMedia(false)
+      if (query === '(prefers-color-scheme: light)') {
+        return mockMatchMedia(true)
       }
 
       return mockMatchMedia(false)
@@ -30,67 +30,99 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
-describe('ExampleComponent', () => {
-  it('don`t render when display is undefined', () => {
-    render(<DarkModeComponent />)
-    const el = screen.queryByTestId('darkmode-component')
+describe('DarkmodeComponent', () => {
+  describe('component render', () => {
+    it('don`t render when display is undefined', () => {
+      render(<DarkModeComponent />)
+      const el = screen.queryByTestId('darkmode-component')
 
-    expect(el).not.toBeInTheDocument()
-  })
+      expect(el).not.toBeInTheDocument()
+    })
 
-  it('render when display is fixed and have the correct class', () => {
-    render(<DarkModeComponent display="fixed" />)
-    const el = screen.queryByTestId('darkmode-component')
-
-    expect(el).toBeInTheDocument()
-    expect(el).toHaveClass('fixed')
-  })
-
-  it('render when display is static and have the correct class', () => {
-    render(<DarkModeComponent display="static" />)
-    const el = screen.queryByTestId('darkmode-component')
-
-    expect(el).toBeInTheDocument()
-    expect(el).toHaveClass('static')
-  })
-
-  const positionYCases: ('top' | 'bottom')[] = ['top', 'bottom']
-
-  it.each(positionYCases)(
-    'render when display is static and positionY is %s and have the correct class',
-    (positionY) => {
-      render(<DarkModeComponent display="static" positionY={positionY} />)
+    it('render when display is fixed and have the correct class', () => {
+      render(<DarkModeComponent display="fixed" />)
       const el = screen.queryByTestId('darkmode-component')
 
       expect(el).toBeInTheDocument()
-      expect(el).toHaveClass(`static ${positionY}-4`)
-    }
-  )
+      expect(el).toHaveClass('fixed')
+    })
 
-  const positionXCases: ('left' | 'right')[] = ['left', 'right']
-
-  it.each(positionXCases)(
-    'render when display is static and positionX is %s and have the correct class',
-    (positionX) => {
-      render(<DarkModeComponent display="static" positionX={positionX} />)
+    it('render when display is static and have the correct class', () => {
+      render(<DarkModeComponent display="static" />)
       const el = screen.queryByTestId('darkmode-component')
 
       expect(el).toBeInTheDocument()
-      expect(el).toHaveClass(`static ${positionX}-4`)
-    }
-  )
+      expect(el).toHaveClass('static')
+    })
 
-  it('render when display is fixed, positionY is center and positionX is center and have the correct class', () => {
-    render(
-      <DarkModeComponent
-        display="fixed"
-        positionY="center"
-        positionX="center"
-      />
+    const positionYCases: ('top' | 'bottom')[] = ['top', 'bottom']
+
+    it.each(positionYCases)(
+      'render when display is static and positionY is %s and have the correct class',
+      (positionY) => {
+        render(<DarkModeComponent display="static" positionY={positionY} />)
+        const el = screen.queryByTestId('darkmode-component')
+
+        expect(el).toBeInTheDocument()
+        expect(el).toHaveClass(`static ${positionY}-4`)
+      }
     )
-    const el = screen.queryByTestId('darkmode-component')
 
-    expect(el).toBeInTheDocument()
-    expect(el).toHaveClass('top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2')
+    const positionXCases: ('left' | 'right')[] = ['left', 'right']
+
+    it.each(positionXCases)(
+      'render when display is static and positionX is %s and have the correct class',
+      (positionX) => {
+        render(<DarkModeComponent display="static" positionX={positionX} />)
+        const el = screen.queryByTestId('darkmode-component')
+
+        expect(el).toBeInTheDocument()
+        expect(el).toHaveClass(`static ${positionX}-4`)
+      }
+    )
+
+    it('render when display is fixed, positionY is center and positionX is center and have the correct class', () => {
+      render(
+        <DarkModeComponent
+          display="fixed"
+          positionY="center"
+          positionX="center"
+        />
+      )
+      const el = screen.queryByTestId('darkmode-component')
+
+      expect(el).toBeInTheDocument()
+      expect(el).toHaveClass(
+        'top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2'
+      )
+    })
+  })
+
+  describe('component behavior', () => {
+    it('should not have the dark class when the component is mounted and the user prefers light mode', async () => {
+      render(<DarkModeComponent />)
+
+      const documentElement = document.documentElement
+
+      expect(documentElement).not.toHaveClass('dark')
+    })
+
+    it('should have the dark class when the component is mounted and the user prefers dark mode', async () => {
+      const matchMediaMock = window.matchMedia as jest.Mock
+
+      matchMediaMock.mockImplementation((query) => {
+        if (query === '(prefers-color-scheme: dark)') {
+          return mockMatchMedia(true)
+        }
+
+        return mockMatchMedia(false)
+      })
+
+      render(<DarkModeComponent />)
+
+      const documentElement = document.documentElement
+
+      expect(documentElement).toHaveClass('dark')
+    })
   })
 })
